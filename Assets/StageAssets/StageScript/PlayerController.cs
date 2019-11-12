b3be572cc1e6eea8d3193c7a7b6c71d1ce9b4c42
カウピーTWICE大好き <gensikaku@gmail.com>
2019-11-13 02:40:39 +0900

    Effect4

--- Assets/StageAssets/StageScript/PlayerController.cs
+++ Assets/StageAssets/StageScript/PlayerController.cs
@@ -8,16 +8,62 @@ public class PlayerController : MonoBehaviour
     public float accel = 5;
 
     private Rigidbody rgbody;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rgbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
         rgbody.AddForce(Vector3.forward * shipSpeed);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.up * accel;
+            animator.SetBool("Up", true);
+        }
+        else
+        {
+            animator.SetBool("Up", false);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= transform.up * accel;
+            animator.SetBool("Down", true);
+        }
+        else
+        {
+            animator.SetBool("Down", false);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.right * accel;
+            animator.SetBool("Right", true);
+        }
+        else
+        {
+            animator.SetBool("Right", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= transform.right * accel;
+            animator.SetBool("Left", true);
+        }
+        else
+        {
+            animator.SetBool("Left", false);
+        }
     }
 }
+
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+//    }
+//}
