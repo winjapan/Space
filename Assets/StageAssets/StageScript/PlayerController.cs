ee055b227ed23fb746015d2d4f06b92763974fab
カウピーTWICE大好き <gensikaku@gmail.com>
2019-11-13 01:51:46 +0900

    Effect

---
+++ Assets/StageAssets/StageScript/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float shipSpeed = 20000;
+    public float accel = 5;
+
+    private Rigidbody rgbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgbody = GetComponent<Rigidbody>();
+        rgbody.AddForce(Vector3.forward * shipSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
