72a3d8c61758b2d805567ae0e7cd43e3785bd659
カウピーTWICE大好き <gensikaku@gmail.com>
2019-11-16 01:45:41 +0900

    Colony

--- Assets/SoundDirector.cs
+++ Assets/SoundDirector.cs
@@ -8,22 +8,42 @@ public class SoundDirector : MonoBehaviour
     [SerializeField]
     private AudioMixerSnapshot gamesound;
     [SerializeField]
-    private AudioMixer audioMixer;
-   
+    UnityEngine.Audio.AudioMixer mixer;
+    UnityEngine.Audio.AudioMixerSnapshot shot;
+    public GameObject Director;
+    AudioSource audioSource;
 
-  
-    public void SetMaster(float  volume)
+    void Awake()
     {
-        audioMixer.SetFloat("Master",volume);
+        audioSource = GetComponent<AudioSource>();
     }
 
-    public void SetBGM(float volume)
+    public void Update()
     {
-        audioMixer.SetFloat("BGM", volume);
+        float bgm = 0.0f;
     }
 
-    public void SetSE(float volume)
+    // Start is called before the first frame update
+    public void SetMaster(float valume)
     {
-        audioMixer.SetFloat("SE", volume);
+        mixer.SetFloat("FieldMaster", valume);
+    }
+
+    public void SetBGM(float valume)
+    {
+        mixer.SetFloat("FieldBGM", valume);
+    }
+    public void SetSE(float valume)
+    {
+        mixer.SetFloat("FieldSE", valume);
+
+
+        Invoke("OFFSound", 1f);
+        audioSource.enabled = true;
+    }
+
+    void OFFSound()
+    {
+        audioSource.enabled = false;
     }
 }
