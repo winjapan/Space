a10a54e1c075539f9a38181441bac98be44df043
カウピーTWICE大好き <gensikaku@gmail.com>
2019-11-26 11:10:58 +0900

    Warp Star

---
+++ Assets/StageAssets/StageScript/RetuneToTitleforDemo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetuneToTitleforDemo : MonoBehaviour
+{
+    public AudioClip button;
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnButtonClicked()
+    {
+        audioSource.PlayOneShot(button);
+        Invoke("Reset", 0.5f);
+    }
+
+    private void Reset()
+    {
+        SceneManager.LoadScene("GameScene for Demo");
+    }
+}
